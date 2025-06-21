b509cdde2c0dbef2bdf98f9e0ca9ec4e4e8c1a90
Ikna Shillingford <73978446+iknals@users.noreply.github.com>
2025-06-20 21:05:19 -0500

    6/20/25 Commit 1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,27 +19,11 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
-          ios: {
-            position: 'absolute',
-          },
+          ios: { position: 'absolute' },
           default: {},
         }),
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
       <Tabs.Screen
         name="timeline"
         options={{
@@ -57,4 +41,3 @@ export default function TabLayout() {
     </Tabs>
   );
 }
-
